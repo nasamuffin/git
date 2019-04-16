b9a24522ada55e4c39b0f92acf198048e74923d7
Emily Shaffer <emilyshaffer@google.com>
2019-04-16 16:58:35 -0700

    walken: add infrastructure for revwalk demo
    Begin to add scaffolding for `git walken`, a toy command which we will
    teach to perform a number of revision walks, in order to demonstrate the
    mechanics of revision walking for developers new to the Git project.
    
    This commit is the beginning of an educational series which correspond
    to the tutorial in Documentation/MyFirstRevWalk.txt.
    
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>

---
+++ builtin/walken.c
@@ -0,0 +1,14 @@
+/*
+ * "git walken"
+ *
+ * Part of the "My First Revision Walk" tutorial.
+ */
+
+#include <stdio.h>
+#include "builtin.h"
+
+int cmd_walken(int argc, const char **argv, const char *prefix)
+{
+	printf(_("cmd_walken incoming...\n"));
+	return 0;
+}
