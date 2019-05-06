cefd8d4b68ccfaf46214a295738650068c5a1f07
Emily Shaffer <emilyshaffer@google.com>
2019-05-06 16:44:32 -0700

    walken: add handler to git_config
    For now, we have no configuration options we want to set up for
    ourselves, but in the future we may need to. At the very least, we
    should invoke git_default_config() for each config option; we will do so
    inside of a skeleton config callback so that we know where to add
    configuration handling later on when we need it.
    
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>

--- builtin/walken.c
+++ builtin/walken.c
@@ -5,6 +5,7 @@
  */
 
 #include "builtin.h"
+#include "config.h"
 #include "parse-options.h"
 
 
@@ -16,9 +17,34 @@
 static void init_walken_defaults(void)
 {
 	/*
-	 * We don't actually need the same components `git log` does; leave this
-	 * empty for now.
+	 * We don't use any other components or have settings to initialize, so
+	 * leave this empty.
 	 */
+}
+
+/*
+ * This method will be called back by git_config(). It is used to gather values
+ * from the configuration files available to Git.
+ *
+ * Each time git_config() finds a configuration file entry, it calls this
+ * callback. Then, this function should compare it to entries which concern us,
+ * and make settings changes as necessary.
+ *
+ * If we are called with a config setting we care about, we should use one of
+ * the helpers which exist in config.h to pull out the value for ourselves, i.e.
+ * git_config_string(...) or git_config_bool(...).
+ *
+ * If we don't match anything, we should pass it along to another stakeholder
+ * who may otherwise care - in log's case, grep, gpg, and diff-ui. For our case,
+ * we'll ignore everybody else.
+ */
+static int git_walken_config(const char *var, const char *value, void *cb)
+{
+	/*
+	 * For now, we don't have any custom configuration, so fall back on the
+	 * default config.
+	 */
+	return git_default_config(var, value, cb);
 }
 
 int cmd_walken(int argc, const char **argv, const char *prefix)
@@ -43,6 +69,8 @@ int cmd_walken(int argc, const char **argv, const char *prefix)
 	argc = parse_options(argc, argv, prefix, options, walken_usage, 0);
 
 	init_walken_defaults();
+
+	git_config(git_walken_config, NULL);
 
 	/*
 	 * This line is "human-readable" and we are writing a plumbing command,
