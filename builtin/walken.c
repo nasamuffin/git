b6649bd35fc8912a1d9222875d59ecda66494077
Emily Shaffer <emilyshaffer@google.com>
2019-05-06 18:20:38 -0700

    walken: add usage to enable -h
    It's expected that Git commands support '-h' in order to provide a
    consistent user experience (and this expectation is enforced by the
    test suite). '-h' is captured by parse_options() by default; in order to
    support this flag, we add a short usage text to walken.c and invoke
    parse_options().
    
    With this change, we can now add cmd_walken to the builtins set and
    expect tests to pass, so we'll do so - cmd_walken is now open for
    business.
    
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>
    Change-Id: I2919dc1efadb82acb335617ea24371c84b03bbce

--- builtin/walken.c
+++ builtin/walken.c
@@ -5,9 +5,30 @@
  */
 
 #include "builtin.h"
+#include "parse-options.h"
+
 
 int cmd_walken(int argc, const char **argv, const char *prefix)
 {
+	/*
+	 * All builtins are expected to provide a usage to provide a consistent user
+	 * experience.
+	 */
+	const char * const walken_usage[] = {
+		N_("git walken"),
+		NULL,
+	};
+
+	struct option options[] = {
+		OPT_END()
+	};
+
+	/*
+	 * parse_options() handles showing usage if incorrect options are
+	 * provided, or if '-h' is passed.
+	 */
+	argc = parse_options(argc, argv, prefix, options, walken_usage, 0);
+
 	/*
 	 * This line is "human-readable" and we are writing a plumbing command,
 	 * so we localize it and use the trace library to print only when
