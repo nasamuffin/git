0b8e61ea90712652e64bd82cefc133588a7caac8
Emily Shaffer <emilyshaffer@google.com>
2019-05-06 18:20:38 -0700

    walken: add usage to enable -h
    One requirement of the Git test suite is that all commands support '-h',
    which is captured by parse_options(). In order to support this flag, add
    a short usage text to walken.c and invoke parse_options().
    
    With this change, we can now add cmd_walken to the builtins set and
    expect tests to pass, so we'll do so - cmd_walken is now open for
    business.
    
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>

--- builtin/walken.c
+++ builtin/walken.c
@@ -6,9 +6,21 @@
 
 #include <stdio.h>
 #include "builtin.h"
+#include "parse-options.h"
+
+static const char * const walken_usage[] = {
+	N_("git walken"),
+	NULL,
+};
 
 int cmd_walken(int argc, const char **argv, const char *prefix)
 {
+	struct option options[] = {
+		OPT_END()
+	};
+
+	argc = parse_options(argc, argv, prefix, options, walken_usage, 0);
+
 	printf(_("cmd_walken incoming...\n"));
 	return 0;
 }
